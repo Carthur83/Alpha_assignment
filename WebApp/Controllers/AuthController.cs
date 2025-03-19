0b711d2de8402d5ab63c155aa22fb38bbd87874e
Carthur83 <christian_A_83@hotmail.com>
2025-03-19 10:10:13 +0100

    Added create account form, log in form and main layout

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -1,18 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class AuthController : Controller
     {
-
-        public IActionResult Register()
+        public IActionResult CreateAccount()
         {
+            var formData = new CreateAccountFormModel();
+            return View(formData);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAccount(CreateAccountFormModel formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(formData);
+            }
+
             return View();
         }
 
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string test)
+        {
+            return RedirectToAction("Projects", "Projects");
         }
     }
 }
---
+++ WebApp/wwwroot/js/validate.js
@@ -0,0 +1,40 @@
+﻿const validateField = (field) => {
+    let spanError = document.querySelector(`span[data-valmsg-for='${field.name}']`)
+    if (!spanError) return;
+
+    let errorMessage = ""
+    let value = field.value.trim()
+
+    if (field.hasAttribute("data-val-required") && value === "")
+        errorMessage = field.getAttribute("data-val-required")
+
+    if (field.hasAttribute("data-val-regex") && value !== "") {
+        let pattern = new RegExp(field.getAttribute("data-val-regex-pattern"))
+        if (!pattern.test(value))
+            errorMessage = field.getAttribute("data-val-regex")
+    }
+
+    if (errorMessage) {
+        spanError.classList.remove("field-validation-valid")
+        spanError.classList.add("field-validation-error")
+        spanError.textContent = errorMessage
+    } else {
+        spanError.classList.add("field-validation-valid")
+        spanError.classList.remove("field-validation-error")
+        spanError.textContent = ""
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form")
+
+    if (!form) return;
+
+    const fields = form.querySelectorAll("input[data-val='true']")
+
+    fields.forEach(field => {
+        field.addEventListener("input", function () {
+            validateField(field)
+        })
+    })
+})
