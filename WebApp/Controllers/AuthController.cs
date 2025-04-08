d1cd4881f5edb8b4d6ecfa39badf2f2edf45c4c0
Carthur83 <christian_A_83@hotmail.com>
2025-04-08 15:32:31 +0200

    Added Admin Login Layout

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -3,77 +3,82 @@ using Business.Interfaces;
 using Data.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.SqlServer.Server;
 using WebApp.Models;
 
-namespace WebApp.Controllers
+namespace WebApp.Controllers;
+
+public class AuthController(IMemberService memberService, SignInManager<MemberEntity> signInManager) : Controller
 {
-    public class AuthController(IMemberService memberService, SignInManager<MemberEntity> signInManager) : Controller
+    private readonly IMemberService _memberService = memberService;
+    private readonly SignInManager<MemberEntity> _signInManager = signInManager;
+
+    public IActionResult CreateAccount()
     {
-        private readonly IMemberService _memberService = memberService;
-        private readonly SignInManager<MemberEntity> _signInManager = signInManager;
+        var formData = new MemberSignUpForm();
+        return View(formData);
+    }
 
-        public IActionResult CreateAccount()
+    [HttpPost]
+    public async Task<IActionResult> CreateAccount(MemberSignUpForm formData)
+    {
+        if (!ModelState.IsValid)
         {
-            var formData = new MemberSignUpForm();
             return View(formData);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateAccount(MemberSignUpForm formData)
+        if (await _memberService.ExistsAsync(formData.Email))
         {
-            if (!ModelState.IsValid)
-            {
-                return View(formData);
-            }
-
-            if (await _memberService.ExistsAsync(formData.Email))
-            {
-                ModelState.AddModelError("Exists", "User Already Exists");
-                return View(formData);
-            }
-
-            var memberRegistrationForm = new MemberRegistrationForm
-            {
-                FirstName = formData.FirstName,
-                LastName = formData.LastName,
-                Email = formData.Email,
-                Password = formData.Password
-            };
-
-            var result = await _memberService.CreateMemberAsync(memberRegistrationForm);
-            if (result)
-                return RedirectToAction("LogIn", "Auth");
-
-            ModelState.AddModelError("NotCreated", "User not created");
+            ModelState.AddModelError("Exists", "User Already Exists");
             return View(formData);
         }
 
-        public IActionResult Login()
+        var memberRegistrationForm = new MemberRegistrationForm
         {
-            return View();
-        }
+            FirstName = formData.FirstName,
+            LastName = formData.LastName,
+            Email = formData.Email,
+            Password = formData.Password
+        };
 
-        [HttpPost]
-        public async Task<IActionResult> Login(MemberLogInForm form)
-        {
-            if (ModelState.IsValid)
-             {
-                var result = await _signInManager.PasswordSignInAsync(form.Email, form.Password, false, false);
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Projects", "Projects", new { status = "all" });
-                }
+        var result = await _memberService.CreateMemberAsync(memberRegistrationForm);
+        if (result)
+            return RedirectToAction("LogIn", "Auth");
+
+        ModelState.AddModelError("NotCreated", "User not created");
+        return View(formData);
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(MemberLogInForm form)
+    {
+        if (ModelState.IsValid)
+         {
+            var result = await _signInManager.PasswordSignInAsync(form.Email, form.Password, false, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Projects", "Projects", new { status = "all" });
             }
-
-            ViewBag.ErrorMessage = "Incorrect email or password";
-            return View(form);
         }
 
-        public async Task<IActionResult> LogOut()
-        {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Login", "Auth");
-        }
+        ViewBag.ErrorMessage = "Incorrect email or password";
+        return View(form);
+    }
+
+    public async Task<IActionResult> LogOut()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Login", "Auth");
+    }
+
+    public IActionResult AdminLogIn()
+    {
+        return View();
     }
 }
+
+
